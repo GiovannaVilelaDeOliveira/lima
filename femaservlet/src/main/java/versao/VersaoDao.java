30b03bc1bd401ec2543153a7fb5de11f524c2ffd
Giovanna Vilela <129780384+GiovannaVilelaDeOliveira@users.noreply.github.com>
2025-10-31 19:58:09 -0300

    Update VersaoDao.java
    aa

--- femaservlet/src/main/java/versao/VersaoDao.java
+++ femaservlet/src/main/java/versao/VersaoDao.java
@@ -11,11 +11,19 @@ private static List<Versao> versaoList = new ArrayList<Versao>();
 	public void cadastrar(Versao pratoInformado) {
 		versaoList.add(pratoInformado);
 	}
-	
+	public void atualizar(Versao pratoInformado) {
+        excluir(pratoInformado.getCodigo());
+		versaoList.add(pratoInformado);
+	}
+
 	public List<Versao> consultar(){
 		return versaoList;
 	}
-	
+    public Versao buscarPorCodigo(Integer id) {
+        return versaoList.stream()
+                .filter(prato -> prato.getCodigo() == id)
+                .collect(Collectors.toList()).get(0);
+    }
 	public void excluir(Integer id) {
 		versaoList = versaoList.stream()
 				.filter(prato -> prato.getCodigo() != id)
